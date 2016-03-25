cdc178f6fcb78def5b4120395c8ca78175aa164a
Kornel Lesiński <kornel@geekhood.net>
2016-03-25 20:14:59 +0000

    Reusable DB helper

--- import.js
+++ import.js
@@ -1,43 +1,19 @@
 'use strict';
 
-const co = require('co');
-const sqlite = require('sqlite3');
-const Queue = require('promise-queue');
 const denodeify = require('denodeify');
+const co = require('co');
 const fs = require('fs');
 const readFile = denodeify(fs.readFile);
 const stat = denodeify(fs.stat);
 const getDimensionsSync = require('image-size');
 const getDimensions = denodeify(getDimensionsSync);
-
 const yr = require('./lib/yr');
+
 const Metadata = yr.Metadata;
 const metadata = new Metadata();
 
-const dbQueue = new Queue(1);
-
-co(function*(){
-    const db = yield new Promise((resolve, reject) => {
-        const db = new sqlite.Database('images.db', err => {
-            if (err) return reject(err);
-            resolve(db);
-        });
-    });
-
-    yield dbExec(db, `CREATE TABLE IF NOT EXISTS images(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        sha1 TEXT UNIQUE NOT NULL,
-        lic TEXT NOT NULL,
-        size INTEGER,
-        width INTEGER,
-        height INTEGER,
-        json TEXT NOT NULL)`);
-    yield dbExec(db, "CREATE TABLE IF NOT EXISTS tags(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT UNIQUE NOT NULL)");
-    yield dbExec(db, "CREATE TABLE IF NOT EXISTS image_tags(tag_id INTEGER NOT NULL, image_id INTEGER NOT NULL, PRIMARY KEY(image_id, tag_id))");
-    yield dbExec(db, "CREATE UNIQUE INDEX IF NOT EXISTS tags_by_tag ON image_tags(tag_id, image_id)");
-
-    yield metadata.forEach({
-        progress: true,
-    }, image => {
+yr.Database.open().then(db => {
+    return metadata.forEach({progress: true}, image => {
         return gatherImageMetadata(image)
             .then(data => putIntoDatabase(db, data));
     });
@@ -52,6 +28,7 @@ function gatherImageMetadata(image) {
         const data = {
             json: image.json(),
             lic: image.data.lic,
+            tags: image.data.tags,
             sha1: image.data.sha1,
         };
 
@@ -81,28 +58,18 @@ function gatherImageMetadata(image) {
 }
 
 function putIntoDatabase(db, data) {
-    return dbQueue.add(co.wrap(function*() {
-
-        yield dbExec(db, "BEGIN");
-        try {
-            const existing = yield dbGet(db, "SELECT id FROM images WHERE sha1 = ?", [data.sha1]);
-            let id;
-            const cols = [data.lic, data.size, data.width, data.height, data.json, data.sha1];
-            if (existing.length) {
-                id = existing[0].id;
-                yield dbExec(db, "UPDATE images SET lic=?, size=?, width=?, height=?, json=? WHERE sha1=?", cols);
-            } else {
-                const insert = yield dbExec(db, "INSERT INTO images(lic,size,width,height,json,sha1) VALUES(?,?,?,?,?,?,?)", cols);
-                id = insert.lastID;
-            }
-
-            yield addTags(db, id, data.tags);
-            yield dbExec(db, "COMMIT");
+    return db.transaction(co.wrap(function*() {
+        const existing = yield db.get1("SELECT id FROM images WHERE sha1 = ?", [data.sha1]);
+        let id;
+        const cols = [data.lic, data.size, data.width, data.height, data.json, data.sha1];
+        if (existing) {
+            id = existing.id;
+            yield db.exec("UPDATE images SET lic=?, size=?, width=?, height=?, json=? WHERE sha1=?", cols);
+        } else {
+            const insert = yield db.exec("INSERT INTO images(lic,size,width,height,json,sha1) VALUES(?,?,?,?,?,?,?)", cols);
+            id = insert.lastID;
         }
-        catch(err) {
-            console.error((err && err.stack) || err);
-            yield dbExec(db, "ROLLBACK");
-        }
+        yield addTags(db, id, data.tags);
     }));
 }
 
@@ -111,26 +78,6 @@ function addTags(db, id, tags) {
         return Promise.resolve();
     }
 
-    return Promise.all(tags.map(tag => dbExec(db, "INSERT OR IGNORE into tags(name) VALUES(?)", [tag])))
-    .then(() => Promise.all(tags.map(tag => dbExec(db, "INSERT OR IGNORE into image_tags(tag_id, image_id) VALUES((SELECT id FROM tags WHERE name=?), ?)", [tag, id]))));
-}
-
-function dbExec(db, query, args) {
-    if (!args) args = [];
-
-    return new Promise((resolve, reject) => {
-        db.run(query, args, function(err) {
-            if (err) reject(err); else resolve(this);
-        });
-    });
-}
-
-function dbGet(db, query, args) {
-    if (!args) args = [];
-
-    return new Promise((resolve, reject) => {
-        db.all(query, args, function(err, data) {
-            if (err) reject(err); else resolve(data);
-        });
-    });
+    return Promise.all(tags.map(tag => db.exec("INSERT OR IGNORE into tags(name) VALUES(?)", [tag])))
+    .then(() => Promise.all(tags.map(tag => db.exec("INSERT OR IGNORE into image_tags(tag_id, image_id) VALUES((SELECT id FROM tags WHERE name=?), ?)", [tag, id]))));
 }
